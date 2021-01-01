c499cd30d1ffb092806b31c0c2b672d1b65fc32c
TEC <tec@tecosaur.com>
2021-01-01 19:23:12 +0800

    Update package commentary

--- lexic.el
+++ lexic.el
@@ -26,7 +26,10 @@
 
 ;;; Commentary:
 
-;; This is a major mode to view output of dictionary search of lexic.
+;; This provides a major mode to view the output of dictionary tools,
+;; and utilities that perform searches and nicely format the results.
+;;
+;; Currently tied to sdcv, but this is intended to be changed in the future.
 
 ;; Put this file into your load-path and the following into your
 ;; ~/.emacs:
